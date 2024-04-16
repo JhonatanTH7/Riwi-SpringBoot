634e8bc80243a7ad784f5d255ff53fc73f202a63
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-04-16 09:11:21 -0500

    New Project First WebPage

---
+++ Projects/FirstWebPage/src/main/java/com/riwi/FirstWebPage/FirstWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.FirstWebPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebPageApplication.class, args);
+	}
+
+}
---
+++ Projects/FirstWebPage/src/test/java/com/riwi/FirstWebPage/FirstWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.FirstWebPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
