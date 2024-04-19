f26b62ebe5183ee2c9d107e613541ec2d8074c40
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-04-19 07:51:10 -0500

    Update

--- FirstWebPage/src/main/java/com/riwi/FirstWebPage/services/CoderService.java
+++ FirstWebPage/src/main/java/com/riwi/FirstWebPage/services/CoderService.java
@@ -1,6 +1,6 @@
 package com.riwi.FirstWebPage.services;
 
-import java.util.List;
+// import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,9 +20,11 @@ public class CoderService {
     private CoderRepository objCoderRepository;
 
     // Servicio para obtener todos los coders
-    public List<Coder> findAll() {
-        return this.objCoderRepository.findAll();
-    }
+    // No necesario después de listado paginado
+
+    // public List<Coder> findAll() {
+    // return this.objCoderRepository.findAll();
+    // }
 
     // Servicio para guardar un coder
     public Coder insert(Coder objCoder) {
