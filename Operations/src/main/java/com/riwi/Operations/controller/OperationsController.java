f18e8933e1d4c74d925521cdf8dd205d098edbfe
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-04-22 08:17:27 -0500

    API operations with POST Method

---
+++ Operations/src/main/java/com/riwi/Operations/controller/OperationsController.java
@@ -0,0 +1,43 @@
+package com.riwi.Operations.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riwi.Operations.entities.Operations;
+
+@RestController
+@RequestMapping("/api/v1/operations")
+public class OperationsController {
+
+    @PostMapping(path = "/sum")
+    public String sum(@RequestBody Operations objOperations) {
+        String result = String.valueOf(objOperations.getNum1() + objOperations.getNum2());
+        return " -->  " + objOperations.getNum1() + " + " + objOperations.getNum2() + " = " + result;
+    }
+
+    @PostMapping(path = "/subtract")
+    public String subtract(@RequestBody Operations objOperations) {
+        String result = String.valueOf(objOperations.getNum1() - objOperations.getNum2());
+        return " -->  " + objOperations.getNum1() + " - " + objOperations.getNum2() + " = " + result;
+    }
+
+    @PostMapping(path = "/multiplication")
+    public String multiplication(@RequestBody Operations objOperations) {
+        String result = String.valueOf(objOperations.getNum1() * objOperations.getNum2());
+        return " -->  " + objOperations.getNum1() + " x " + objOperations.getNum2() + " = " + result;
+    }
+
+    @PostMapping(path = "/division")
+    public String division(@RequestBody Operations objOperations) {
+        String result = " -->  ";
+        if (objOperations.getNum2() != 0) {
+            result = String.valueOf(objOperations.getNum1() / objOperations.getNum2());
+            return " -->  " + objOperations.getNum1() + " / " + objOperations.getNum2() + " = " + result;
+        } else {
+            result = "division by zero not allowed";
+        }
+        return result;
+    }
+}
---
+++ Operations/src/main/java/com/riwi/Operations/entities/Operations.java
@@ -0,0 +1,33 @@
+package com.riwi.Operations.entities;
+
+public class Operations {
+    private double num1;
+    private double num2;
+
+    public Operations(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    @Override
+    public String toString() {
+        return " - Number 1 = " + num1 + "   Num 2 = " + num2;
+    }
+
+}
