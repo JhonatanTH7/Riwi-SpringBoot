d90809b9ef0505ba21d18d07fdaad2aee56580ec
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-05-06 09:26:35 -0500

    Update with entity relations

--- BeautySalon/src/main/java/com/riwi/BeautySalon/domain/entities/ClientEntity.java
+++ BeautySalon/src/main/java/com/riwi/BeautySalon/domain/entities/ClientEntity.java
@@ -1,14 +1,21 @@
 package com.riwi.BeautySalon.domain.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name = "clients")
 @Data
@@ -28,4 +35,9 @@ public class ClientEntity {
     private String phone;
     @Column(length = 100, nullable = false)
     private String email;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<Appointment> appointments;
 }
